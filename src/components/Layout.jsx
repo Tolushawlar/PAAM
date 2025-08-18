fafdb14af6fd1f2750f931bd411ad1a32ec24d2e
flo-mg <iadebayo2002@gmail.com>
2025-08-18 16:37:29 +0100

    Creation of InfoCard and InfoCard for dashboard

--- src/components/Layout.jsx
+++ src/components/Layout.jsx
@@ -2,6 +2,7 @@ import React from 'react'
 import { useLocation } from 'react-router-dom'
 import Sidebar from './Sidebar'
 import Navbar from './Navbar'
+import InfoCard from './InfoCard'
 
 export default function Layout({ children }) {
   const location = useLocation()
